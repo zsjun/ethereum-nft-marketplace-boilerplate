5cefe6498b5a109e7920e83adf00a3197bed4f35
zsj <zousjun@126.com>
2023-03-01 09:19:13 +0800

    change dir 2 client and server

--- server/src/index.js
+++ server/src/index.js
@@ -19,17 +19,18 @@ app.use(
 const MORALIS_API_KEY =
   "TbMhuBTwPYG053aXGln3a8FGI6NshUJ6PF4DgYRm0LewbeWSzIoYwRxNuAOUKlsl";
 const address = "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d";
+const chain = EvmChain.ETHEREUM;
 
 app.get("/balances", async (req, res) => {
   try {
     // Promise.all() for receiving data async from two endpoints
     const [nativeBalance, tokenBalances] = await Promise.all([
       Moralis.EvmApi.balance.getNativeBalance({
-        chain: EvmChain.ETHEREUM,
+        chain,
         address,
       }),
       Moralis.EvmApi.token.getWalletTokenBalances({
-        chain: EvmChain.ETHEREUM,
+        chain,
         address,
       }),
     ]);
@@ -47,9 +48,66 @@ app.get("/balances", async (req, res) => {
   }
 });
 
+async function getDemoData() {
+  // Get native balance
+  const nativeBalance = await Moralis.EvmApi.balance.getNativeBalance({
+    address,
+    chain,
+  });
+
+  // Format the native balance formatted in ether via the .ether getter
+  const native = nativeBalance.result.balance.ether;
+
+  // Get token balances
+  const tokenBalances = await Moralis.EvmApi.token.getWalletTokenBalances({
+    address,
+    chain,
+  });
+
+  // Format the balances to a readable output with the .display() method
+  const tokens = tokenBalances.result.map((token) => token.display());
+
+  // Get the nfts
+  const nftsBalances = await Moralis.EvmApi.nft.getWalletNFTs({
+    address,
+    chain,
+    limit: 10,
+  });
+
+  // Format the output to return name, amount and metadata
+  const nfts = nftsBalances.result.map((nft) => ({
+    name: nft.result.name,
+    amount: nft.result.amount,
+    metadata: nft.result.metadata,
+  }));
+
+  return { native, tokens, nfts };
+}
+
+app.get("/demo", async (req, res) => {
+  try {
+    // Get and return the crypto data
+    const data = await getDemoData();
+    res.status(200);
+    res.json(data);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500);
+    res.json({ error: error.message });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 const startServer = async () => {
   await Moralis.start({
     apiKey: MORALIS_API_KEY,
+    formatEvmAddress: "checksum",
+    formatEvmChainId: "decimal",
+    logLevel: "verbose",
   });
 
   app.listen(port, () => {
